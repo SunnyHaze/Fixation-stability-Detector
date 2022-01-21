47ebd77972c9bad759b18c460e028b7c6bd0f38a
SunnyHaze <mxch1122@126.com>
2022-01-22 01:30:29 +0800

    添加必要注释

--- getPixelPerCM.py
+++ getPixelPerCM.py
@@ -22,31 +22,32 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 screen.fill((255,255,255)) # 填充白色背景
 # 设置窗口标题
 pygame.display.set_caption("欢迎校正本系统")
+# 输出必要指导性文字
 font = pygame.font.SysFont(pygame.font.get_fonts(),FontSize) 
 text1 = font.render("Please make sure the distance between two red lines is 5cm to Calibrate the system.",True,(0,0,0))
 text2 = font.render("After that, type ENTER button on the keyboard to SAVE the settings and quit!",True,(0,0,0))
 
-LineLocation = 0
+LineLocation = 0 # 第二条红线的坐标
 clock = pygame.time.Clock()
 while True:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN: # 鼠标按下更新坐标
             LineLocation = event.pos[0]
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
+            if event.key == pygame.K_RETURN: # 按下Enter后更新测量长度到文件，并推出脚本
                 settings["pixelPerCM"] = abs(WIDTH/4 - LineLocation) / 5
                 print("The new pixelPerCM is {}.".format(settings["pixelPerCM"]))
                 with open("config.json","w",encoding="utf-8") as f:
                     f.write(json.dumps(settings))
                 sys.exit()
+    # 初始填充            
     screen.fill((255,255,255)) # 填充白色背景
     screen.blit(text1,(10,10))
     screen.blit(text2,(10,10 + FontSize))
     pygame.draw.rect(screen,(255,0,0),(WIDTH/4,0,1,HEIGHT),0)
     pygame.draw.rect(screen,(255,0,0),(LineLocation,0,1,HEIGHT),0)
-            
     pygame.display.flip()
 pygame.quit()
--- visionStable.py
+++ visionStable.py
@@ -3,18 +3,10 @@ from encodings import utf_8
 import json
 from multiprocessing.connection import wait
 import sys
-from turtle import width
 import pygame
 import random
 
-from scipy import rand
 # 导入配置文件
-
-def cross(surface,color,location,size):
-    scaleRate = 0.1
-    barWidth = scaleRate * size
-    pygame.draw.rect(surface,color,(location[0]-barWidth/2,location[1]-size/2,barWidth,size),0)
-    pygame.draw.rect(surface,color,(location[0]-size/2,location[1]-barWidth/2,size,barWidth),0)
 with open("config.json","r",encoding="utf-8") as f:
     str = f.read()
     settings = json.loads(str)
@@ -34,12 +26,19 @@ with open("config.json","r",encoding="utf-8") as f:
     blackPointDiameterAngle = settings["blackPointDiameterAngle"]
 
 # 利用给定的参数实现视角到像素的转换
-def angle2Pixel(angle):
+def angle2Pixel(angle): # 利用三角函数实现角度到长度的映射
     return tan(angle * 2 * pi / 360) * distanceOfSubject * pixelPerCM
+
+# 绘制中央黑十字
+def cross(surface,color,location,size):
+    scaleRate = 0.1
+    barWidth = scaleRate * size
+    pygame.draw.rect(surface,color,(location[0]-barWidth/2,location[1]-size/2,barWidth,size),0)
+    pygame.draw.rect(surface,color,(location[0]-size/2,location[1]-barWidth/2,size,barWidth),0)
 
 blackPointWidth = int(angle2Pixel(blackPointDiameterAngle)) # 计算黑点直径
 # 绘制中央黑点
-def drawCenterPoint(surface):
+def drawCenterPoint(surface): #
     pygame.draw.circle(surface,(0,0,0),(WIDTH/2,HEIGHT/2),blackPointWidth/2)
 
 # 绘制干扰点
@@ -57,8 +56,9 @@ def drawDistract(surface):
     dx = ranLength * cos(ranAngle) + starCorrection[0]
     dy = ranLength * sin(ranAngle) + starCorrection[1]
     starLocation =  WIDTH/2 + dx, HEIGHT/2 + dy
-    surface.blit(star,starLocation)
-          
+    surface.blit(star,starLocation) # 绘制
+
+# 按时间序列生成一系列10个混淆点    
 def DistractVision(surface):
     timeArray = [random.random()*5 for i in range(20)]
     timeArray.sort()
@@ -73,6 +73,7 @@ def DistractVision(surface):
         drawCenterPoint(surface)
         pygame.display.flip()
         endTime = i[1]
+
 # 管理整个15000s流程
 def theWholeProcess(surface):
     # 先展示5秒十字
@@ -87,8 +88,10 @@ def theWholeProcess(surface):
     # 展示混淆
     DistractVision(surface)
     pygame.time.wait(5000)
+    pygame.quit()
     exit(0)
-# 初始化
+
+# 初始化Pygame
 pygame.init()
 # 设置主屏幕
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -98,13 +101,9 @@ pygame.display.set_caption("欢迎来到本测试")
 
 clock = pygame.time.Clock()
 while True:
-    clock.tick(60)
-
+    clock.tick(60) # 设置刷新率，不过没什么用
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    # pygame.draw.circle(screen, (0,0,0), (WIDTH/2, HEIGHT/2), pixelPerCM * blackCircleDiameter / 2, width=0)        
-    
-    # cross(screen,(0,0,0),(WIDTH/2, HEIGHT/2), pixelPerCM * blackCrossDiameter)
-    theWholeProcess(screen) 
-pygame.quit()
+    theWholeProcess(screen)  # 执行全渲染流程即可，完事儿后退出
+
